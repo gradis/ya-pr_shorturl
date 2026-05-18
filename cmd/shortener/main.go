6f34244dc0db37e8614ba23199fbcc192736f5de
Dmitry Zh. <everfree.gradis@gmail.com>
2026-05-18 15:50:06 +0300

    setted up server

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,24 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	"github.com/gradis/ya-pr_shorturl/internal/handler"
+	"github.com/gradis/ya-pr_shorturl/internal/repository"
+	"github.com/gradis/ya-pr_shorturl/internal/service"
+)
+
+func main() {
+	repo := repository.NewMemoryRepository()
+	urlService := service.NewURLService(repo)
+	urlHandler := handler.NewURLHandler(urlService)
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", urlHandler.Handle)
+
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		panic(err)
+	}
+}
